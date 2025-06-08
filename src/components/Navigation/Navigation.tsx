b20066eb54f9620411fce2536a7ac1eb16d9345d
fishystock <215427190+fishystock@users.noreply.github.com>
2025-06-08 18:44:36 -0400

    Navigation & configure generate-react-cli

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+interface NavigationProps {}
+
+const Navigation: FC<NavigationProps> = () => (
+  <div>
+    Navigation Component
+  </div>
+);
+
+export default Navigation;
