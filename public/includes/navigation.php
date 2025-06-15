29f6770bd2988fa3f69cd17c5a66ccd3c694c5a5
fishystock <215427190+fishystock@users.noreply.github.com>
2025-06-14 20:30:45 -0400

    Clickable header

--- public/includes/navigation.php
+++ public/includes/navigation.php
@@ -1,12 +1,14 @@
 <!-- Mobile Header -->
 <header class="bg-slate-900 text-white w-full flex items-center justify-between p-4 md:hidden">
-    <div class="flex items-center gap-4">
-        <img src="/assets/logo.png" class="w-10 h-10">
+    <a href="/" class="mb-0">
+        <div class="flex items-center gap-4">
+                <img src="/assets/logo.png" class="w-10 h-10">
 
-        <!-- Automation title truncation for mobile support -->
-        <h1 class="min-sm:inline hidden roboto-slab font-bold text-lg">Sheridan Server Administration Society</h1>
-        <h1 class="min-sm:hidden inline roboto-slab font-bold text-lg">Server Administration Society</h1>
-    </div>
+                <!-- Automation title truncation for mobile support -->
+                <h1 class="min-sm:inline hidden roboto-slab font-bold text-lg">Sheridan Server Administration Society</h1>
+                <h1 class="min-sm:hidden inline roboto-slab font-bold text-lg">Server Administration Society</h1>
+        </div>
+    </a>
     <button id="menu-toggle" class="text-white text-2xl focus:outline-none">
         <i class="fa fa-bars"></i>
     </button>
@@ -30,12 +32,14 @@
 
 <!-- Desktop Sidebar -->
 <aside class="hidden md:flex fixed top-0 left-0 h-full w-sm flex-col space-y-6 border-slate-800 border-r-2 bg-slate-900">
-    <div class="p-6 mb-0 text-center">
-        <div class="flex gap-5">
-            <img src="/assets/logo.png" class="w-14 h-14 mx-auto">
-            <h1 class="roboto-slab text-white font-bold text-xl">Sheridan Server Administration Society</h1>
+    <a href="/" class="mb-0">
+        <div class="p-6 mb-0 text-center">
+            <div class="flex gap-5">
+                <img src="/assets/logo.png" class="w-14 h-14 mx-auto">
+                <h1 class="roboto-slab text-white font-bold text-xl">Sheridan Server Administration Society</h1>
+            </div>
         </div>
-    </div>
+    </a>
     <a href="/" class="text-white px-6 py-3 mb-0 text-xl roboto-slab hover:bg-gradient-to-r hover:from-slate-900 hover:to-slate-800">
         <i class="fa fa-sharp fa-house w-9"></i> Home
     </a>
