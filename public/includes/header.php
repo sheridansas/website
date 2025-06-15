b6b0363e7afbd45e14eef4fd98ca9ec1852c09d8
fishystock <215427190+fishystock@users.noreply.github.com>
2025-06-14 21:34:50 -0400

    Update meta tags

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,5 +1,16 @@
+<title>Sheridan Server Administration Society</title>
+
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="title" content="Sheridan Server Administration Society">
+<meta name="description" content=" The Sheridan Server Administration Society brings together students who are passionate about self-hosting, OSS, Linux, and all things tech related. ">
+<meta name="keywords" content="server, sysadmin, sheridan, sheridan college, club, workshop, lessons, computers, programming, linux, oss">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+<meta name="revisit-after" content="5 days">
+
+
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 <link rel="stylesheet" href="/css/fontawesome.css">
 <link rel="stylesheet" href="/css/index.css">
