e16fe891bd64408ad16dc310065a8482f87e5fdc
fishystock <215427190+fishystock@users.noreply.github.com>
2025-06-15 22:59:56 -0400

    Move .env outside of public
    PHP and beer should outlawed, didn't push this to prod!

--- public/includes/database.php
+++ public/includes/database.php
@@ -1,5 +1,5 @@
 <?php
-$env = parse_ini_file(".env");
+$env = parse_ini_file("../.env");
 $conn = pg_connect(
     "host={$env["DATABASE_HOST"]} port={$env["DATABASE_PORT"]} dbname=sas user={$env["DATABASE_USER"]} password={$env["DATABASE_PASSWORD"]}"
 );
