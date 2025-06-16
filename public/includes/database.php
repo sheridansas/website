5431eb34150cad779d1c1de68743c71f61ec440f
fishystock <215427190+fishystock@users.noreply.github.com>
2025-06-15 23:04:57 -0400

    Code aesthetics

--- public/includes/database.php
+++ public/includes/database.php
@@ -1,6 +1,6 @@
 <?php
 $env = parse_ini_file("../.env");
 $conn = pg_connect(
-    "host={$env["DATABASE_HOST"]} port={$env["DATABASE_PORT"]} dbname=sas user={$env["DATABASE_USER"]} password={$env["DATABASE_PASSWORD"]}"
+    "host={$env["DATABASE_HOST"]} port={$env["DATABASE_PORT"]} dbname=sas user={$env["DATABASE_USER"]} password={$env["DATABASE_PASS"]}"
 );
 ?>
